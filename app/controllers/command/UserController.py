98b00f0e4e810457e7a84d90f4143b60929c2f65
icyfirecz <tkrivanek2@gmail.com>
2023-11-09 10:09:57 +0100

    [UPDATE] project structure cleanup

---
+++ app/controllers/command/UserController.py
@@ -0,0 +1,50 @@
+from litestar import delete, get, post, put
+from uuid import UUID
+from litestar.controller import Controller
+from litestar.di import Provide
+from litestar.status_codes import HTTP_200_OK
+from app.controllers.provide_services import provides_user_service
+from app.models.command_model import ReadDTO, UserModel, WriteDTO
+from app.utils.controller import Service
+from app.utils.pika import RabbitMQConnection
+
+
+# UserModel controller which handles requests for /users
+class UserController(Controller):
+    # Dependency injection
+    dto = WriteDTO
+    return_dto = ReadDTO
+    dependencies = {"service": Provide(provides_user_service, sync_to_thread=False)}
+
+    @get(path="/users")
+    async def get_users(self, service: Service) -> list[UserModel]:
+        return await service.list()
+
+    @get(path="/users/{user_id:uuid}")
+    async def get_user(self, service: Service, user_id: UUID) -> UserModel:
+        return await service.get(user_id)
+
+    @post(path="/users")
+    async def create_user(self, service: Service, data: UserModel) -> UserModel:
+        new_user = await service.create(data)
+        await service.repository.session.commit()
+        with RabbitMQConnection() as conn:
+            conn.publish_message('hello', UserModel.to_dict(new_user, 'post'))
+
+        return new_user
+
+    @put(path="/users/{user_id:uuid}")
+    async def update_user(self, data: UserModel, service: Service, user_id: UUID) -> UserModel:
+        updated_user = await service.update(user_id, data)
+        await service.repository.session.commit()
+        with RabbitMQConnection() as conn:
+            conn.publish_message('hello', UserModel.to_dict(updated_user, 'put'))
+        return updated_user
+
+    @delete(path="/users/{user_id:uuid}", status_code=HTTP_200_OK)
+    async def delete_user(self, service: Service, user_id: UUID) -> UserModel:
+        deleted_user = await service.delete(user_id)
+        await service.repository.session.commit()
+        with RabbitMQConnection() as conn:
+            conn.publish_message('hello', UserModel.to_dict(deleted_user, 'delete'))
+        return deleted_user
--- app/controllers/command_controller.py
+++ app/controllers/command_controller.py
@@ -1,25 +1,13 @@
-import os
-import sys
-from uuid import UUID
-
-from litestar import Litestar, delete, get, post, put
+from litestar import Litestar, get
 from litestar.contrib.sqlalchemy.base import UUIDBase
 from litestar.contrib.sqlalchemy.plugins.init import SQLAlchemyInitPlugin
 from litestar.contrib.sqlalchemy.plugins.init.config import (
     AsyncSessionConfig,
     SQLAlchemyAsyncConfig,
 )
-from litestar.controller import Controller
-from litestar.di import Provide
-from litestar.status_codes import HTTP_200_OK
-from sqlalchemy.ext.asyncio import AsyncSession
 
-# TODO: Fix imports
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from ..models.command_model import ReadDTO, UserModel, UserRepository, WriteDTO
-from ..utils.controller import Service
-from ..utils.pika import RabbitMQConnection
-
+from app.utils.pika import RabbitMQConnection
+from app.controllers.command.UserController import UserController
 
 @get("/")
 async def hello_world() -> str:
@@ -27,53 +15,6 @@ async def hello_world() -> str:
     with RabbitMQConnection() as conn:
         conn.publish_message('hello', {'message': 'Hello from Controller!'})
     return "Hello, Controller!"
-
-
-# Service dependency
-def provides_service(db_session: AsyncSession) -> Service:
-    """Constructs repository and service objects for the request."""
-    return Service(UserRepository(session=db_session))
-
-
-# UserModel controller which handles requests for /users
-class UserController(Controller):
-    # Dependency injection
-    dto = WriteDTO
-    return_dto = ReadDTO
-    dependencies = {"service": Provide(provides_service, sync_to_thread=False)}
-
-    @get(path="/users")
-    async def get_users(self, service: Service) -> list[UserModel]:
-        return await service.list()
-
-    @get(path="/users/{user_id:uuid}")
-    async def get_user(self, service: Service, user_id: UUID) -> UserModel:
-        return await service.get(user_id)
-
-    @post(path="/users")
-    async def create_user(self, service: Service, data: UserModel) -> UserModel:
-        new_user = await service.create(data)
-        await service.repository.session.commit()
-        with RabbitMQConnection() as conn:
-            conn.publish_message('hello', UserModel.to_dict(new_user, 'post'))
-
-        return new_user
-
-    @put(path="/users/{user_id:uuid}")
-    async def update_user(self, data: UserModel, service: Service, user_id: UUID) -> UserModel:
-        updated_user = await service.update(user_id, data)
-        await service.repository.session.commit()
-        with RabbitMQConnection() as conn:
-            conn.publish_message('hello', UserModel.to_dict(updated_user, 'put'))
-        return updated_user
-
-    @delete(path="/users/{user_id:uuid}", status_code=HTTP_200_OK)
-    async def delete_user(self, service: Service, user_id: UUID) -> UserModel:
-        deleted_user = await service.delete(user_id)
-        await service.repository.session.commit()
-        with RabbitMQConnection() as conn:
-            conn.publish_message('hello', UserModel.to_dict(deleted_user, 'delete'))
-        return deleted_user
 
 
 # PostgreSQL connection string: postgresql+asyncpg://admin:admin@localhost:5432/db_name
